9d27c1649cab49627350bc88c021193a47f111ce
venkatesh-devale-sjsu <venkatesh.devale@sjsu.edu>
2018-04-12 01:45:18 -0700

    added back ground image for the world and added actor image

--- BlackPanther/BlackPanther/BlackPanther.java
+++ BlackPanther/BlackPanther/BlackPanther.java
@@ -16,6 +16,6 @@ public class BlackPanther extends World
     public BlackPanther()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(852, 480, 1); 
     }
 }
