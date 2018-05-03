c64b443bd27f05117942de583dc0fa60e2877d59
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2018-05-03 13:22:29 -0700

    Added GameOverLevel2
    Added GameOverLevel2

---
+++ BlackPanther/GameOverLevel2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOverLevel2 here.
+ * 
+ * @author (Pradnyesh) 
+ * @version (a version number or a date)
+ */
+public class GameOverLevel2 extends Actor
+{
+    /**
+     * Act - do whatever the GameOverLevel2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
