07a19837f6e62b7ebef25f3dfb28bfc22c1de988
shrishrimal-rohit <rohit.shrishrimal@sjsu.edu>
2018-05-03 13:27:45 -0700

    hurdle changed

--- BlackPanther/Hurdle.java
+++ BlackPanther/Hurdle.java
@@ -12,8 +12,12 @@ public class Hurdles extends Actor
      * Act - do whatever the Hurdles wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int Hur_Speed = -8;
+   int Hur_Speed1 = -5;
     public void act() 
     {
         // Add your action code here.
+        setLocation(getX() + Hur_Speed, getY());
     }    
+    
 }
