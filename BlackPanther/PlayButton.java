65ee4f1f03488503b067c6c71960ce71ffdf9edc
RajivYadav01 <33535411+RajivYadav01@users.noreply.github.com>
2018-05-03 13:40:48 -0700

    Updated PlayButton

--- BlackPanther/PlayButton.java
+++ BlackPanther/PlayButton.java
@@ -7,6 +7,19 @@ import greenfoot.*;
  */
 public class PlayButton extends Button implements Invoker  
 {
- 
+  private Command PlayCommand;
+    public PlayButton(String title){
+        
+    }
+   
+    public void invoke()
+    {
+         if(Greenfoot.mouseClicked(this))
+         PlayCommand.execute();
+    }
+     public void setCommand(Command c)
+     {
+          this.PlayCommand=c;
+     }
    
 }
