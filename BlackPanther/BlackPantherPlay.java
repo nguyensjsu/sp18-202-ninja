426eec866bb963a005b6ed8ce10c11ee15fa8566
shrishrimal-rohit <rohit.shrishrimal@sjsu.edu>
2018-05-03 12:55:44 -0700

    added the state check & live checker for the actor

---
+++ BlackPanther/BlackPantherPlay.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class BlackPantherPlay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BlackPantherPlay  
+{
+   public static void main(String[] args){
+		BlackPantherHero blackPantherHero= new BlackPantherHero();
+		blackPantherHero.getHasLivesState();
+		blackPantherHero.getNoLivesState();
+	}
+}
